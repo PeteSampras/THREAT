076f0bf56120bea4f65401436919e067c953e184
PeteSampras <PeteSampras.mmobugs@gmail.com>
2019-05-07 14:00:32 -0400

    placeholder main file

---
+++ threat/threat.py
@@ -0,0 +1,12 @@
+import sys
+
+def threat()
+  pass
+
+if __name__=='__main__':
+    try:
+        threat()
+    except KeyboardInterrupt:
+        print("Keyboard interrupted")
+    finally:
+        sys.exit()

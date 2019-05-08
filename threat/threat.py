1f1d1e7835df0fc9a892680172cf7448febca3a7
PeteSampras <PeteSampras.mmobugs@gmail.com>
2019-05-07 18:30:55 -1000

    Update threat.py

--- threat/threat.py
+++ threat/threat.py
@@ -1,7 +1,36 @@
+#!/usr/bin/env python
 import sys
+from core.build_menu import buildmenu
 
-def threat()
-  pass
+menu = { # '#' : ['module', 'description', 'function']
+        '1':['Reconnaissance & OSINT','Description','recon'],\
+        '2':['Scanning & Enumeration','Description','scanenum'],\
+        '3':['Vulnerability Analysis','Description','vulnysis'],\
+        '4':['Exploitation','Description','exploitation'],\
+        '5':['Post Analysis','Description','post']\
+    }
+
+target=[]
+
+class Target:
+    def __init__(self,name,current_menu,last_menu,main_menu):
+        self.name = name
+        self.current_menu = current_menu
+        self.last_menu = last_menu
+        self.main_menu = main_menu
+
+def threat():
+    while True:
+        try:
+            host = 'www.example.com'# DEBUG: temp value
+            current_menu = menu
+            last_menu = menu
+            target.append(Target(host,current_menu,last_menu,menu))
+            buildmenu(target,menu,'Main Menu','')
+        except KeyboardInterrupt:
+            print("Keyboard interrupted")
+        finally:
+            sys.exit()
 
 if __name__=='__main__':
     try:
@@ -10,3 +39,4 @@ if __name__=='__main__':
         print("Keyboard interrupted")
     finally:
         sys.exit()
+
